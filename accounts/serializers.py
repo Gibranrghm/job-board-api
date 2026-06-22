ace2e1f91880eea359a57398d9efe600c272d259
Gibranrghm <gibran.r.hdz@gmail.com>
2026-06-21 22:34:18 -0500

    Step 3: Add JWT authentication with register, login, and profile endpoints

---
+++ accounts/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password2 = serializers.CharField(write_only=True, label='Confirm password')
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'password2', 'role']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': 'Passwords do not match.'})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role', 'bio', 'location', 'website']
+        read_only_fields = ['id']
